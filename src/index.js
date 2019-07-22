511f76d6faca2151bac9fcbbf513660d75bc2ebd
Leonardo Matos <leomp120894@gmail.com>
2019-07-22 20:41:52 -0300

    feat(current-object): get current page object from store api first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log(1)
+import { _config } from '@ecomplus/utils'
+import { mapBySlug, apiStore } from '@ecomplus/client'
+
+export default slug => new Promise((resolve, reject) => {
+  // try to get current document info from global E-Com config first
+  let resource = _config.get('page_resource')
+  let objectId = _config.get('page_object_id')
+
+  const getCurrentObject = () => {
+    // get current page object from Store API
+    apiStore(`/${resource}/${objectId}.json`)
+      .then(({ data }) => resolve(data))
+      .catch(reject)
+  }
+
+  if (resource && objectId) {
+    getCurrentObject()
+  } else {
+    // map page resource and object based on received slug or window location
+    mapBySlug(slug)
+      .then(page => {
+        resource = page.resource
+        objectId = page.object_id
+        getCurrentObject()
+      })
+      .catch(reject)
+  }
+})
